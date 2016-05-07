04434d77231da81ed792c9568073755f0e893e39
Daniel Cunnington <dancunnington@uk.ibm.com>
2016-05-07 14:15:18 +0200

    map

--- bluemix-dan-personality-insights/public/main.js
+++ bluemix-dan-personality-insights/public/main.js
@@ -35,25 +35,42 @@ $(document).ready(function() {
 
         $.post('/findEvents', eventQuery, function(events) {
             var newEntry;
+            var locations = [];
             for(var i = 0; i < events.length; i++){
                 if(i == events.length - 1){
                     newEntry = '<div class="result last" onmouseover="this.style.background=\'#ffffff\';" onmouseout="this.style.background=\'transparent\';">';
                 }else{
                     newEntry = '<div class="result" onmouseover="this.style.background=\'#ffffff\';" onmouseout="this.style.background=\'transparent\';">';
                 }
-                newEntry = newEntry  + '<span class="title">' + events[i].name + '</span>';
+                newEntry = newEntry  + '<p class="title">' + events[i].name + '</p>';
                 newEntry = newEntry  + '</div>';
                 $("#results").append(newEntry);
+                //locations.push(L.marker(events[i].))
                 console.log(events[i]);
             }
             stepForward(3);
+
+            $("#container").hide();
+            $(".final-results").show();
+
+            var map = L.map('map').setView([43.710100, 7.261997], 13);
+
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            
         });
 
         //get personality insights also
         if (twitterhandle != "") {
           $.get('/addUser/'+twitterhandle, function(res) {
             $.get('/matchUser/'+twitterhandle, function(users) {
-                console.log(users);
+                users = users.matches;
+                for (var i=0; i<users.length; i++) {
+                    var html = "<div class='user'><p>@<a target='_blank' href='http://twitter.com/"+users[i].twitterhandle+"'>"+users[i].twitterhandle+"</a></p></div>";
+                    $(".users").append(html);
+                }
             });
           }); 
         }
